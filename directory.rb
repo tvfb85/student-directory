52f6d71e5455714e47e36fb0ffa1fc16665e7f46
Vicky Roberts <vicki_uk@hotmail.com>
2017-01-31 07:21:14 -0600

    Referencing the data using hash keys

--- directory.rb
+++ directory.rb
@@ -1,3 +1,38 @@
+students = [
+  {name: "Dr. Hannibal Lecter", cohort: :november},
+  {name: "Darth Vader", cohort: :november},
+  {name: "Nurse Ratched", cohort: :november},
+  {name: "Michael Corleone", cohort: :november},
+  {name: "Alex DeLarge", cohort: :november},
+  {name: "The Wicked Witch of the West", cohort: :november},
+  {name: "Terminator", cohort: :november},
+  {name: "Freddy Krueger", cohort: :november},
+  {name: "The Joker", cohort: :november},
+  {name: "Joffrey Baratheon", cohort: :november},
+  {name: "Norman Bates", cohort: :november}
+]
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+print_header
+print(students)
+print_footer(students)
+
+=begin
+------------------------------------------------------
 # student_count = 11
 
 students = [
@@ -33,7 +68,7 @@ print_header
 print(students)
 print_footer(students)
 
-=begin
+
 ------------------------------------------------------
 student_count = 11
 
