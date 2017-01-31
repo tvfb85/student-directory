5f05e206b2341959784a6b42e0712917cdb6eb58
Vicky Roberts <vicki_uk@hotmail.com>
2017-01-31 07:35:55 -0600

    Adding user input

--- directory.rb
+++ directory.rb
@@ -1,3 +1,45 @@
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  # create the empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this code
+  while !name.empty? do
+    # add the student hash to the array
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
+  end
+  # return the array of students
+  students
+end
+
+def print_header
+  puts "The students of Villains Academy"
+  puts "------------"
+end
+
+def print(students)
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  end
+end
+
+def print_footer(names)
+  puts "Overall, we have #{names.count} great students"
+end
+
+students = input_students
+print_header
+print(students)
+print_footer(students)
+
+
+=begin
+-----------------------------------------------------
 students = [
   {name: "Dr. Hannibal Lecter", cohort: :november},
   {name: "Darth Vader", cohort: :november},
@@ -31,7 +73,7 @@ print_header
 print(students)
 print_footer(students)
 
-=begin
+
 ------------------------------------------------------
 # student_count = 11
 
